b0299f75798ff8809f5e46e1472442ed161ca016
aidsplus <68637723+aidsplus@users.noreply.github.com>
2020-08-03 18:41:45 -0300

    Update valores.js

--- valores.js
+++ valores.js
@@ -1,1 +1,3 @@
-var raros = { url: "JS1", nome: "John", atual: "JS2", anterior: "JS3", atualizado: "JS4", tipo: "JS5", categoria: "JS6", valor: "JS7" };
+var raros = {};
+
+raros['bletplus'] = { url: "JS1", nome: "John", atual: "JS2", anterior: "JS3", atualizado: "JS4", tipo: "JS5", categoria: "JS6", valor: "JS7" };
