d4a865579093ec75d12c08f22210f3f9b86fec85
aidsplus <68637723+aidsplus@users.noreply.github.com>
2020-08-03 08:48:51 -0300

    Update valores.js

--- valores.js
+++ valores.js
@@ -1,9 +1,9 @@
 var raros = {};
 
-raros['1'] = { nome: 'totem', atual: 10 };
-raros['2'] = { nome: 'totem marrom', atual: 30 };
-raros['3'] = { nome: 'totem branco', atual: 20 };
-raros['4'] = { nome: 'totem azul', atual: 19 };
-raros['5'] = { nome: 'totem verde', atual: 13 };
-raros['6'] = { nome: 'totem amarelo', atual: 14 };
-raros['7'] = { nome: 'totem vermelho', atual: 12 };
+raros['r1'] = { nome: 'totem', atual: 10 };
+raros['r2'] = { nome: 'totem marrom', atual: 30 };
+raros['r3'] = { nome: 'totem branco', atual: 20 };
+raros['r4'] = { nome: 'totem azul', atual: 19 };
+raros['r5'] = { nome: 'totem verde', atual: 13 };
+raros['r6'] = { nome: 'totem amarelo', atual: 14 };
+raros['r7'] = { nome: 'totem vermelho', atual: 12 };
