e43e814a60dfe0a049d3d9a519cf26f96c235af9
aidsplus <68637723+aidsplus@users.noreply.github.com>
2020-08-03 18:48:01 -0300

    Update valores.js

--- valores.js
+++ valores.js
@@ -1,3 +1,9 @@
-var raros = {};
+ var salas = {};
 
-raros['bletplus'] = { url: "JS1", nome: "John", atual: "JS2", anterior: "JS3", atualizado: "JS4", tipo: "JS5", categoria: "JS6", valor: "JS7" };
+salas['alimentos'] = { nome: 'alimentos', icone: '🍉', arr: alimentos };
+salas['animais'] = { nome: 'animais', icone: '🐶', arr: animais };
+salas['bandeiras'] = { nome: 'bandeiras', icone: '🚩', arr: bandeiras };
+salas['desenho_animado'] = { nome: 'desenho_animado', icone: '👾', arr: desenho_animado };
+salas['got'] = { nome: 'got', icone: '🐲', arr: got };
+salas['objetos'] = { nome: 'objetos', icone: '📷', arr: objetos };
+salas['verbos'] = { nome: 'verbos', icone: '🏃', arr: verbos };
