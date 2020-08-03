a738e4a07281ba52368908fcfa26c787fc4657d2
aidsplus <68637723+aidsplus@users.noreply.github.com>
2020-08-03 18:32:16 -0300

    Update valores.js

--- valores.js
+++ valores.js
@@ -1,9 +1,1 @@
-var raros = {};
-
-raros['r1'] = { nome: 'totem', atual: 10 };
-raros['r2'] = { nome: 'totem marrom', atual: 30 };
-raros['r3'] = { nome: 'totem branco', atual: 20 };
-raros['r4'] = { nome: 'totem azul', atual: 19 };
-raros['r5'] = { nome: 'totem verde', atual: 13 };
-raros['r6'] = { nome: 'totem amarelo', atual: 14 };
-raros['r7'] = { nome: 'totem vermelho', atual: 12 };
+var r1 = { url: "JS1", nome: "John", atual: "JS2", anterior: "JS3", anterior: "JS4", atualizado: "JS5", tipo: "JS6", categoria: "JS7", valor: "JS8" };
