62b475a4029c203944c7308b75cc888d9298348b
aidsplus <68637723+aidsplus@users.noreply.github.com>
2020-08-03 19:20:01 -0300

    Update valores.js

--- valores.js
+++ valores.js
@@ -1,9 +1,1 @@
- var salas = {};
-
-salas['alimentos'] = { nome: 'alimentos', icone: '🍉', arr: alimentos };
-salas['animais'] = { nome: 'animais', icone: '🐶', arr: animais };
-salas['bandeiras'] = { nome: 'bandeiras', icone: '🚩', arr: bandeiras };
-salas['desenho_animado'] = { nome: 'desenho_animado', icone: '👾', arr: desenho_animado };
-salas['got'] = { nome: 'got', icone: '🐲', arr: got };
-salas['objetos'] = { nome: 'objetos', icone: '📷', arr: objetos };
-salas['verbos'] = { nome: 'verbos', icone: '🏃', arr: verbos };
+ var r1 = {url: 'alimentos', nome: 'alimentos', atual: 'alimentos', anterior: '🍉',};
