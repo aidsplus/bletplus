c4052d901dbaaadc13a275dc6395f26c850d7210
aidsplus <68637723+aidsplus@users.noreply.github.com>
2020-08-03 18:38:02 -0300

    Update valores.js

--- valores.js
+++ valores.js
@@ -1,1 +1,1 @@
-var r1 = { url: "JS1", nome: "John", atual: "JS2", anterior: "JS3", anterior: "JS4", atualizado: "JS5", tipo: "JS6", categoria: "JS7", valor: "JS8" };
+var r1 = { url: "JS1", nome: "John", atual: "JS2", anterior: "JS3", atualizado: "JS4", tipo: "JS5", categoria: "JS6", valor: "JS7" };
